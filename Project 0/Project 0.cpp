e1367fbd859dd7bd21322cd1556dec5d1dcbec8a
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2017-08-31 11:42:24 -0500

    Update Project 0.cpp

--- Project 0/Project 0.cpp
+++ Project 0/Project 0.cpp
@@ -1,3 +1,24 @@
+//********************************************************************
+//      Project #0: Delete Duplicates from an Integer Array
+//      Name: Chris Duhan
+//      Class: Advanced Algorithms and Data Structures
+//      Instructor: Mr. Richard Simpson
+//      Due Date: 09/06/2017
+//********************************************************************
+//      Place your general program documentation here. It should
+//      be quite a few lines explaining the programs duty carefully.
+//      It should also indicate how to run the program and data
+//      input format, filenames etc.
+//********************************************************************
+
+//********************************************************************
+//      ObjectName::MethodName()
+//      Parameters: List them here and comment
+//      Complexity: O(N) (worst case)
+//      A discussion of what the method/function does and required
+//      parameters as well as return value. Also indicate the 
+//      complexity of this function (worst case).
+//********************************************************************
 int list [101];
 
 for(int i = 0; i < x; i++)
