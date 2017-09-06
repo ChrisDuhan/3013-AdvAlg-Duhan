0130aa16d979c4a534640dafeeef0566c7b36abb
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2017-09-05 19:40:15 -0600

    Final project0.cpp
    Final draft

--- Project 0/Project 0.cpp
+++ Project 0/Project 0.cpp
@@ -5,25 +5,24 @@
 //      Instructor: Mr. Richard Simpson
 //      Due Date: 09/06/2017
 //********************************************************************
-//      Place your general program documentation here. It should
-//      be quite a few lines explaining the programs duty carefully.
-//      It should also indicate how to run the program and data
-//      input format, filenames etc.
-//********************************************************************
-
-//********************************************************************
-//      ObjectName::MethodName()
-//      Parameters: List them here and comment
-//      Complexity: O(N) (worst case)
-//      A discussion of what the method/function does and required
-//      parameters as well as return value. Also indicate the 
-//      complexity of this function (worst case).
+//      This program takes a file containing a line of numbers
+//		and checks it for duplicated numbers. It takes 
+//		non-duplicated numbers and stores them in a new array
+//		while ignoring duplicates. The input file must have the
+//		quantity of numbers on the line (minus one) as the first
+//		number on the line. When telling the program the user 
+//		must give the filename as well as the file extension.
+//		It is simple enoungh to do without containing it to
+//		it's own function so the worst case complexity that
+//		the program could experience is O(2n), occuring if there
+//		no unique numbers in the list. Otherwise the complexity
+//		is O(n).
+//
 //********************************************************************
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
@@ -33,37 +32,59 @@ int main()
 	cout << "Please enter the name of the file you wish to manipulate with file extension: " << endl;
 	cin >> fname;
 	ifstream input;
-	ofstream output;
 	input.open(fname);
-	output.open("Chris_Duhan_output.txt");
-	int x = 0;
-	int val = 0;
-	int cnt = 0;
+	int init_array_size = 0;
+	int readin_val = 0;
+	int unique_cnt = 0;
 	int list[101];
-	vector <int> v;
 
 	if (input)
 	{
-		while (input >> x)
+		while (input >> init_array_size)
 		{
-			for (int i = 0; i < x; i++)
+			int * p1;
+			p1 = new int[init_array_size];
+			int p1_pos = 0;
+
+			int unique_cnt = 0;				// reseting for each array
+
+			for (int i = 0; i < init_array_size; i++)
 			{
-				input >> val;
-				if (list[val] != 1)
+				input >> readin_val;
+
+				if (list[readin_val] != 1)
 				{
-					list[val] = 1;
-					v.push_back(val);
-					cout << val << " ";
+					list[readin_val] = 1;
+					p1[p1_pos] = readin_val;
+					unique_cnt++;			// I know p1_pos and unique_cnt are going to 
+					p1_pos++;				// be the same but they handle different things
 				}
 			}
-			cout << endl;
-			int * p;
-			p = new int[v.size()];
-			for (unsigned i = 0; i < v.size(); i++)
+
+			for (int i = 0; i < 101; i++)
 			{
-				p[i] = v[i];
-				list[i] = 0;
+				list[i] = 0;				// clearing for each array
 			}
+
+			int * p2;
+			p2 = new int[unique_cnt];
+
+			for (int i = 0; i < unique_cnt; i++)
+			{
+				p2[i] = p1[i];
+			}
+			
+			delete [] p1;
+			p1 = nullptr;
+
+			for (int i = 0; i < unique_cnt; i++)
+			{
+				cout << p2[i] << " ";
+			}
+			cout << endl;
+
+			delete[] p2;
+			p2 = nullptr;
 		}
 		input.close();
 	}
