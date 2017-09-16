825744bde35a78f0df1d8a56ba5f295866e571ad
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2017-09-16 14:37:55 -0600

    Update LinkedListTest.cpp

--- Project 1/LinkedListTest.cpp
+++ Project 1/LinkedListTest.cpp
@@ -9,7 +9,13 @@ int main()
 	L.push_back(4);
 	L.push_back(5);
 	L.push_back(6);
+	L.push_back(7);
 	L.push_front(111);
 	L.print(0);
+	cout << L.front();
+	L.pop_front();
+	L.pop_back();
+	L.print(3);
+	cout << L.size() << " " << L.empty();
 	return 0;
 }
