aaae7ea196169715ac0162d1871cae64ed6e41cb
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2017-09-28 21:24:22 -0600

    Update SLList.h

--- Project 1/SLList.h
+++ Project 1/SLList.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -19,6 +20,7 @@ class SLList
 
 public:
 	SLList();
+	SLList(const SLList &);
 	~SLList();
 	void push_back(int i);
 	void push_front(int i);
@@ -28,4 +30,34 @@ public:
 	int front();
 	bool empty();
 	int size();
+	//********************************************************************
+	//		SSList::operator=()
+	//		Parameters: const reference to a SLList object
+	//		Complexity: O(1)
+	//		Overloaded compairison operator.
+	//********************************************************************
+	SLList &operator=(const SLList &);
+
+	friend ostream &operator<<(ostream &output, const SLList &L) {
+		Node * t = L._head->_next;
+		//int ct = min(L.size(), 10);
+		//List size = 21 : 2, 5, 4, 76, 1, 9, 7, 12, 54, 88, ... 23
+		output << "List size = " << L._head->_value << " : ";
+		if (L._head->_value > 10) {
+			for (size_t i = 0; i < 10; i++)
+			{
+				output << t->_value << ", ";// leaves a space at end.
+				t = t->_next;//go to next node
+			}
+			cout << "... " << L._tail->_value;
+		}
+		else {
+			while (t != L._tail->_next)
+			{
+				cout << t->_value << ", ";
+				t = t->_next;
+			}
+		}
+		return output;
+	}
 };
