2962c3d8c8f69385b426a73ac33c0f3cfcc0e94e
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2017-10-16 21:39:41 -0600

    Update ResearchBST.cpp

--- Project 3/ResearchBST.cpp
+++ Project 3/ResearchBST.cpp
@@ -109,6 +109,68 @@ void ResearchBST::Sdelete(int val)//3-2
 	SdeleteAux(this->_root, val);
 }
 
+void ResearchBST::PdeleteAux(TreeNodeptr & root, int x)//5-1 currently sdelete still
+{
+	if (root != nullptr)
+	{
+		TreeNodeptr tempPtr, predPtr;
+
+		if (x < root->_value)
+			PdeleteAux(root->_left, x);
+		else if (x > root->_value)
+			PdeleteAux(root->_right, x);
+		else
+		{	// we have found it so lets delete it
+			// tree points at it right!?
+			// If No children we just delete it
+			if (root->_left == nullptr && root->_right == nullptr)
+			{
+				delete(root);
+				root = nullptr; // what happens when we
+				return;       // return here! important!
+			}
+			// Check to see if the node to delete has only one child
+			if (root->_left == nullptr)
+			{	// Then splice in the right side
+				tempPtr = root->_right;
+				delete root;
+				root = tempPtr;
+			}
+			else if (root->_right == nullptr)
+			{	// splice left
+				tempPtr = root->_left;
+				delete(root);
+				root = tempPtr;
+			}
+			else
+				// both children exist! so...
+				// Here root has two children
+				// We first find the successor
+			{
+				tempPtr = root->_left; // Go left
+										// and the all the way to the right
+				while (tempPtr->_right != nullptr)
+				{
+					predPtr = tempPtr;
+					tempPtr = tempPtr->_right;
+				}
+				root->_value = tempPtr->_value; // Copy the value up to the root
+				if (root->_left == tempPtr)
+					root->_left = tempPtr->_left;
+				else
+					predPtr->_right = tempPtr->_left;
+
+				delete tempPtr;
+			}
+		}
+	}
+}
+
+void ResearchBST::Pdelete(int val)//5-2
+{
+	SdeleteAux(this->_root, val);
+}
+
 void ResearchBST::destroyTree(TreeNodeptr & tree)//4-1
 {
 	if (tree != nullptr)
@@ -119,4 +181,3 @@ void ResearchBST::destroyTree(TreeNodeptr & tree)//4-1
 		tree = nullptr;
 	}
 }
-
