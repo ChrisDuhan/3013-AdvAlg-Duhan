902aae3326d2aa141c87876b53c601d101dc4fbe
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2017-10-24 17:43:54 -0500

    Update BSTTest.cpp

--- Project 3/BSTTest.cpp
+++ Project 3/BSTTest.cpp
@@ -5,20 +5,27 @@ using namespace std;
 
 int main()
 {
-	//ResearchBST t(TREESIZE);//create an empty tree with _size=0
+	ResearchBST t(TREESIZE);//create an empty tree with _size=0
+	// The above is redundant?
 	int iterations = 100;
-	cout << "Data for predecessor delete only " << endl;
+	//cout << "Data for predecessor delete only " << endl;
+	// commented out the above as it's not always true in every test
 	vector<long long>iplData(40, 0);// 40 sample ipl values
 	cout << "--------------------------------------------------" << endl;
-	for (int ct = 0; ct < iterations; ct++) 
+	for (int ct = 0; ct < iterations; ct++)
 	{//collect data 100 times to average
 		if (ct % 2 == 0)cout << ".";// A nice trick to see your program running
 		ResearchBST t(TREESIZE);//create an random tree with TREESIZE nodes
-		for (int i = 0; i < INSDELPAIRS; i++) 
+		for (int i = 0; i < INSDELPAIRS; i++)
 		{// INSDELPAIRS is 100,000
 			if (i % 2500 == 0)
 				iplData[i / 2500] += t.IPL();
-			t.RandDelInsPair(1);//Im doing a Pdelete every time here
+			t.RandDelInsPair(1);// 1 for Pdelete, anything else for Sdelete
+			// Use below for alternating delete
+			/*if (i % 2 == 0)
+				t.RandDelInsPair(1);
+			else
+				t.RandDelInsPair(0);*/
 		}
 		// t gets destroyed here when it goes out of scope
 	}
