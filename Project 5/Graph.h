3bed3cefad87208c8b7fd8edc8e3d73727e27eeb
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2017-11-16 21:13:55 -0700

    Update and rename Project5.h to Graph.h

---
+++ Project 5/Graph.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+using namespace std;
+enum dtype { DIRECTED, UNDIRECTED };
+enum color { WHITE, BLACK, GREY };
+class Graph
+{
+	// The wt value will be used in a later algorithm.
+	vector<vector<pair<int, int>>> G; //Pair < node#, wt >
+	vector<color> Color;// Not really required but will reduce parameter passing.
+	dtype dir;
+	void DFS_Visit(int, vector<int>&);
+public:
+	Graph(int, dtype); // resizes G to int and sets dir to type.
+	~Graph();
+	// NOTE:  both AddNewEdge and AddEdgeWeight load both edges if dir = UNDIRECTED
+	void AddNewEdge(int, int);
+	void AddEdgeWeight(int, int, int);
+	vector<int> BFS(int);// return the parent vector (ie the BFS tree)
+	vector<int> DFS(int);// dito
+};
