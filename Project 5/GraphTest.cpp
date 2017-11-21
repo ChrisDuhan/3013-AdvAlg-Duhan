bc46a55a815a27bfc9208282c6ac98b26b412ac7
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2017-11-20 19:48:32 -0700

    Update GraphTest.cpp

--- Project 5/GraphTest.cpp
+++ Project 5/GraphTest.cpp
@@ -7,13 +7,44 @@ int main()
 {
 	int size;
 	char type;
+	vector<int> BFSparent;
+	vector<int> DFSparent;
 
 	cin >> type >> size;
 
 	if (type == 'U')
+	{
 		Graph myG(size, UNDIRECTED);
+		BFSparent = myG.BFS(5);
+		DFSparent = myG.DFS(-1);
+		for (int i = 0; i < BFSparent.size(); ++i)
+		{
+			cout << BFSparent[i] << " ";
+		}
+		cout << endl;
+		for (int i = 0; i < DFSparent.size(); ++i)
+		{
+			cout << DFSparent[i] << " ";
+		}
+	}
 	else
+	{
 		Graph myG(size, DIRECTED);
+		BFSparent = myG.BFS(5);
+		DFSparent = myG.DFS(-1);
+		for (int i = 0; i < BFSparent.size(); ++i)
+		{
+			cout << BFSparent[i] << " ";
+		}
+		cout << endl;
+		for (int i = 0; i < DFSparent.size(); ++i)
+		{
+			cout << DFSparent[i] << " ";
+		}
+	}
+
+	
+	
 	
 
 	return 0;
